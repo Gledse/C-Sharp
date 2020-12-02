2c2bf29fa2b33ee5318458dafe54c0b728f78aee
Gledse_Jamisse <53790917+Gledse@users.noreply.github.com>
2020-12-02 14:05:04 -0800

    nova hospedagem

---
+++ Ex1/Ex1/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace Ex1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.txtDelta = new System.Windows.Forms.TextBox();
+            this.txtValorX2 = new System.Windows.Forms.TextBox();
+            this.txtValorB = new System.Windows.Forms.TextBox();
+            this.txtValorA = new System.Windows.Forms.TextBox();
+            this.txtValorC = new System.Windows.Forms.TextBox();
+            this.txtValorX1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(300, 204);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(171, 23);
+            this.btnLimpar.TabIndex = 0;
+            this.btnLimpar.Text = " Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(28, 204);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(171, 23);
+            this.btnCalcular.TabIndex = 1;
+            this.btnCalcular.Text = "      Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // txtDelta
+            // 
+            this.txtDelta.Location = new System.Drawing.Point(355, 44);
+            this.txtDelta.Name = "txtDelta";
+            this.txtDelta.Size = new System.Drawing.Size(100, 20);
+            this.txtDelta.TabIndex = 2;
+            // 
+            // txtValorX2
+            // 
+            this.txtValorX2.Location = new System.Drawing.Point(355, 126);
+            this.txtValorX2.Name = "txtValorX2";
+            this.txtValorX2.Size = new System.Drawing.Size(100, 20);
+            this.txtValorX2.TabIndex = 3;
+            // 
+            // txtValorB
+            // 
+            this.txtValorB.Location = new System.Drawing.Point(99, 81);
+            this.txtValorB.Name = "txtValorB";
+            this.txtValorB.Size = new System.Drawing.Size(100, 20);
+            this.txtValorB.TabIndex = 4;
+            // 
+            // txtValorA
+            // 
+            this.txtValorA.Location = new System.Drawing.Point(99, 44);
+            this.txtValorA.Name = "txtValorA";
+            this.txtValorA.Size = new System.Drawing.Size(100, 20);
+            this.txtValorA.TabIndex = 5;
+            // 
+            // txtValorC
+            // 
+            this.txtValorC.Location = new System.Drawing.Point(99, 126);
+            this.txtValorC.Name = "txtValorC";
+            this.txtValorC.Size = new System.Drawing.Size(100, 20);
+            this.txtValorC.TabIndex = 6;
+            // 
+            // txtValorX1
+            // 
+            this.txtValorX1.Location = new System.Drawing.Point(355, 81);
+            this.txtValorX1.Name = "txtValorX1";
+            this.txtValorX1.Size = new System.Drawing.Size(100, 20);
+            this.txtValorX1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = " ValorA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = " ValorB";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 129);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = " ValorC";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(286, 47);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = " Delta";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(286, 84);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = " ValorX1";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(286, 129);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = " ValorX2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(551, 284);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtValorX1);
+            this.Controls.Add(this.txtValorC);
+            this.Controls.Add(this.txtValorA);
+            this.Controls.Add(this.txtValorB);
+            this.Controls.Add(this.txtValorX2);
+            this.Controls.Add(this.txtDelta);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.btnLimpar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txtDelta;
+        private System.Windows.Forms.TextBox txtValorX2;
+        private System.Windows.Forms.TextBox txtValorB;
+        private System.Windows.Forms.TextBox txtValorA;
+        private System.Windows.Forms.TextBox txtValorC;
+        private System.Windows.Forms.TextBox txtValorX1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Ex1/Ex1/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ex1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            double a = double.Parse(txtValorA.Text);
+            double b = double.Parse(txtValorB.Text);
+            double c = double.Parse(txtValorC.Text);
+
+            if ((a != 0))
+            {
+               /* if( (a == null) ){
+                    a = 1;
+
+                }*/
+                double d = Math.Pow(-b, 2) - 4 * a * c;
+                txtDelta.Text = d + "";
+                
+                if ((d >= 0))
+                {
+                    double x1 = (-b + Math.Sqrt(d)) / (2 * a);
+                    txtValorX1.Text = x1 + "";
+
+                    double x2 = (-b - Math.Sqrt(d)) /( 2 * a);
+                    txtValorX2.Text = x2 + "";
+
+                } else { MessageBox.Show("A funcao nao tem raizes reias"); }
+            }else { MessageBox.Show("Valor invalido, A e B tem de ser diferente de 0"); }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtValorA.Text = "";
+            txtValorB.Text = "";
+            txtValorC.Text = "";
+            txtDelta.Text = "";
+            txtValorX1.Text = "";
+            txtValorX2.Text = "";
+
+        }
+    }
+}
